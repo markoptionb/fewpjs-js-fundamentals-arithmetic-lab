57c6190671d776e250847908cc730b450be792d5
markoptionb <optionbsupport@gmail.com>
2021-03-07 16:43:08 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 34 ) +1;
+
+const num3 = 8
+const num4 = 2
+
+const mod = num3 / num4
+
+const max = 20
+Math.max(1, 20)
+
